79b3c69b45cf4605a4db5f450e0f6f01d87a3484
Gustavo Vianna Vacilotto <guga.vacilotto@gmail.com>
2022-03-25 17:03:47 -0300

    Variables class

--- Phase1Section34/Program.cs
+++ Phase1Section34/Program.cs
@@ -5,8 +5,21 @@ namespace Practice
     class Program{
         static void Main(String[] args){
 
-            System.Console.WriteLine("test");
-            
+            bool xbool = true;
+            byte xbyte = 255;
+            sbyte xsbyte = -11;
+            char xchar = 'a';
+            decimal xdecimal = 2.3M;
+            double xdouble = 2.3;
+            float xfloat = 2.3f;
+            int xint = -32;
+            uint xuint = 32;
+            long xlong = -64;
+            ulong xulong = 64;
+            object xobject = new object();
+            short xshort = 16;
+            ushort xushort = 16;
+            string xstring = "name";
         }
     }
 }
