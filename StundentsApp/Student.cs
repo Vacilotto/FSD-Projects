06e5ea8b96bfe5c639a275979b99b6bf4e417045
Gustavo Vianna Vacilotto <guga.vacilotto@gmail.com>
2022-03-30 16:44:53 -0300

    Adding the basic file reader from phase 1 sec 4.14

--- StundentsApp/Student.cs
+++ StundentsApp/Student.cs
@@ -1,13 +1,13 @@
 public class Student {
     private string name {get;set;}
-    private int id {get;set;}
+    private string id {get;set;}
     private string classandsection {get;set;}
     public Student (){
         name = "";
-        id = 0;
+        id = "";
         classandsection = "";
     }
-    public Student(string nm, int Id, string clss){
+    public Student(string nm, string Id, string clss){
         name = nm;
         id = Id;
         classandsection = clss;
