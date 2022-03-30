7371fd304546e9a7be947bba3190514f297af8e4
Gustavo Vianna Vacilotto <guga.vacilotto@gmail.com>
2022-03-30 15:15:29 -0300

    Adding phase1 section3.10

---
+++ Phase1section310/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+DoApp();
+
+void DoApp(){
+    char[] s1= {'L','e','t','t','e','r','s'};
+    string stringOfChar = new string(s1);
+
+    System.Console.WriteLine(stringOfChar);
+    foreach (char c in stringOfChar)
+        System.Console.Write(c);
+    System.Console.WriteLine();
+    System.Console.WriteLine(stringOfChar[2]);
+
+    string sentence = "This is a sentence";
+    System.Console.WriteLine(sentence);
+    foreach (char c in sentence)
+        System.Console.Write(c);
+    System.Console.WriteLine();
+    System.Console.WriteLine(sentence[3]);
+
+    sentence += ", that has additional content";
+    System.Console.WriteLine(sentence);
+    foreach (char c in sentence)
+        System.Console.Write(c);
+    System.Console.WriteLine();
+    System.Console.WriteLine(sentence[20]);
+}
---
+++ StundentsApp/Student.cs
@@ -0,0 +1,10 @@
+public class Student {
+    private string name {get;set;}
+    private int id {get;set;}
+    private string classandsection {get;set;}
+    public Student (){
+        name = "";
+        id = 0;
+        classandsection = "";
+    }
+}
