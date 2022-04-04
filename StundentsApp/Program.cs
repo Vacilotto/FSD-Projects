6505c603ef30b65070bcda85b849ec6143faa767
Gustavo Vianna Vacilotto <guga.vacilotto@gmail.com>
2022-04-04 09:29:37 -0300

    Fixed name, adding crud methods in the interface

--- StundentsApp/Program.cs
+++ StundentsApp/Program.cs
@@ -4,45 +4,53 @@ using System.IO;
 
 Main();
 
-void Main(){
-    string fileName = Directory.GetCurrentDirectory()+"\\data.txt";
-    List<Teacher> listTeachers = null;
-    if(File.Exists(fileName)){
+void Main()
+{
+    string fileName = Directory.GetCurrentDirectory() + "\\data.txt";
+    List<Teacher> listTeachers = new List<Teacher>();
+    if (File.Exists(fileName))
+    {
         string[] lines = File.ReadAllLines(fileName);
         listTeachers = generateList(lines);
     }
-    else{System.Console.WriteLine("File isn't present on the specified path.\n"+
-    $"Specified file path: {fileName}");}
-    
+    else
+    {
+        System.Console.WriteLine("File isn't present on the specified path.\n" +
+    $"Specified file path: {fileName}");
+    }
+
     string[] textToFile = genListToSave(listTeachers);
     File.Delete(fileName);
-    
 
-    updateFile(fileName,textToFile);
+    updateFile(fileName, textToFile);
 
 
 }
 
 
 //Method created to read the file and generate a List of Teachers
-List<Teacher> generateList(string[] list){
-    
+List<Teacher> generateList(string[] list)
+{
+
     List<Teacher> lista = new List<Teacher>();
-    
-    foreach(string line in list){
-            string[] str = line.Split(';');
-            Teacher stud = new Teacher(str[0],str[1],str[2]);
-            lista.Add(stud);
-        }
-        lista.RemoveAt(0);
+
+    foreach (string line in list)
+    {
+        string[] str = line.Split(';');
+        Teacher stud = new Teacher(str[0], str[1], str[2]);
+        lista.Add(stud);
+    }
+    lista.RemoveAt(0);
     return lista;
 }
 
 //Method created to generate an array of strings that'll be added in the file to save.
-string[] genListToSave(List<Teacher> list){
+string[] genListToSave(List<Teacher> list)
+{
     List<string> text = new List<string>();
     text.Add("Name;Id;Class");
-    foreach(Teacher stud in list){
+    foreach (Teacher stud in list)
+    {
         string s = $"{stud.name};{stud.id};{stud.classandsection}";
         text.Add(s);
     }
@@ -50,18 +58,17 @@ string[] genListToSave(List<Teacher> list){
 }
 
 //Method create to update the file using the file path and the list of Teachers already in string formats(use genListToSave if needed)
-void updateFile(string path, string[] list){
+void updateFile(string path, string[] list)
+{
     using (StreamWriter sw = File.CreateText(path))
-            {
-                foreach(string s in list){
-                    sw.WriteLine(s);
-                }
-            }	
+    {
+        foreach (string s in list)
+        {
+            sw.WriteLine(s);
+        }
+    }
 
 }
 
-// Method created to search for a specific teacher
-
-// Method created to remove a teacher from the file
 
 // Method created to act as the main menu for the application
---
+++ StundentsApp/TeacherInterface.cs
@@ -0,0 +1,92 @@
+class TeacherInterface
+{
+
+    // Methods created to search for a specific teacher usign 3 different criteria
+
+    // Search Teacher by Name
+    Teacher searchTeacherByName(List<Teacher> list, String name)
+    {
+        foreach (Teacher tc in list)
+            if (tc.name == name)
+            {
+                return tc;
+            }
+        return null;
+    }
+
+    // Search Teacher by ID
+    Teacher searchTeacherById(List<Teacher> list, String id)
+    {
+        foreach (Teacher tc in list)
+            if (tc.id == id)
+            {
+                return tc;
+            }
+        return null;
+    }
+
+    // Search all teachers who works with selected class
+    List<Teacher> searchTeacherByClass(List<Teacher> list, String clas)
+    {
+        List<Teacher> teachers = new List<Teacher>();
+        foreach (Teacher tc in list)
+            if (tc.classandsection == clas)
+            {
+                teachers.Add(tc);
+            }
+        return teachers;
+    }
+
+
+    // Method created to remove a teacher from the file using 3 different criteria to remove
+    List<Teacher> removeByName(List<Teacher> list, string name)
+    {
+        List<Teacher> support = new List<Teacher>();
+        foreach (Teacher tc in list)
+        {
+            if (tc.name == name)
+            {
+                list.Remove(tc);
+            }
+        }
+
+        return support;
+    }
+
+    List<Teacher> removeById(List<Teacher> list, string id)
+    {
+        List<Teacher> support = new List<Teacher>();
+        foreach (Teacher tc in list)
+        {
+            if (tc.id == id)
+            {
+                list.Remove(tc);
+            }
+        }
+
+        return support;
+    }
+
+    List<Teacher> removeByClass(List<Teacher> list, string clas)
+    {
+        List<Teacher> support = new List<Teacher>();
+        foreach (Teacher tc in list)
+        {
+            if (tc.classandsection == clas)
+            {
+                list.Remove(tc);
+            }
+        }
+
+        return support;
+    }
+
+    // Method for creation of new teacher
+    List<Teacher> addTeacher(List<Teacher> list, string name, string id, string clas)
+    {
+        List<Teacher> support = new List<Teacher>();
+        support.Add(new Teacher(name, id, clas));
+
+        return support;
+    }
+}
