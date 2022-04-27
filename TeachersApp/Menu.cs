2a7ed59184f0ae3fd6f42a899440d726bc688a07
Gustavo Vianna Vacilotto <guga.vacilotto@gmail.com>
2022-04-27 08:20:31 -0300

    Adding small breaks to look more natural

--- TeachersApp/Menu.cs
+++ TeachersApp/Menu.cs
@@ -30,6 +30,87 @@ void Main()
 
 }
 
+// Method created to act as the main menu for the application
+void mainMenu()
+{
+    string fileName = Directory.GetCurrentDirectory() + "\\data.txt";
+
+    System.Console.WriteLine("Welcome to Rainbow School system.");
+    System.Console.WriteLine($"We're going to load the Teachers list from {fileName} \n");
+    //Loading the list and storing it in the memory
+    List<Teacher> listTeachers = loadList(fileName);
+
+    //Working in the case of the file being deleted
+    if (listTeachers == null)
+    {
+        System.Console.WriteLine("Oops! It seems the file doesn't exist, do you want to generate a new one?(y/n)");
+        while (true)
+        {
+
+            string ans = Console.ReadLine();
+            if (ans == "y" || ans == "Y")
+            {
+                createFile(fileName);
+            }
+            else if (ans == "n" || ans == "N")
+            {
+                System.Console.WriteLine("Closing the app.");
+                Environment.Exit(1);
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid input, please use y, Y, n or N. Please try again.");
+                System.Console.WriteLine();
+            }
+        }
+    }
+
+    //Executing the main menu options which are: Listing, Updating, Adding and removing.
+    while (true)
+    {
+        System.Console.WriteLine("\nPlease select one of below options:\n 1 - List all teachers\n 2 - Add a new teacher\n 3 - Remove a teacher\n 4 - Update an existing teacher" +
+            "\n 5 - Close application");
+        string ans = Console.ReadLine();
+        if (ans == null || !"12345".Contains(ans))
+        {
+            System.Console.WriteLine("Invalid option, please select one of the valid options below.");
+        }
+        else
+        {
+            switch (ans)
+            {
+                case "1":
+                    System.Console.WriteLine("\n");
+                    Thread.Sleep(1000);
+                    TeacherInterface.listTeachers(listTeachers);
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+                    string[] textToFile = genListToSave(listTeachers);
+                    File.Delete(fileName);
+
+                    updateFile(fileName, textToFile);
+                    Thread.Sleep(1000);
+                    System.Console.WriteLine("Bye bye.");
+                    Environment.Exit(1);
+                    break;
+            }
+        }
+    }
+
+
+
+}
+
+
 //Method to open and read the file, returning a list of teachers
 List<Teacher> loadList(string fileName)
 {
@@ -100,78 +181,3 @@ void createFile(string fileName)
     }
 }
 
-
-// Method created to act as the main menu for the application
-void mainMenu()
-{
-    string fileName = Directory.GetCurrentDirectory() + "\\data.txt";
-
-    System.Console.WriteLine("Welcome to Rainbow School system.");
-    System.Console.WriteLine($"We're going to load the Teachers list from {fileName} \n");
-    //Loading the list and storing it in the memory
-    List<Teacher> listTeachers = loadList(fileName);
-
-    //Working in the case of the file being deleted
-    if (listTeachers == null)
-    {
-        System.Console.WriteLine("Oops! It seems the file doesn't exist, do you want to generate a new one?(y/n)");
-        while (true)
-        {
-
-            string ans = Console.ReadLine();
-            if (ans == "y" || ans == "Y")
-            {
-                createFile(fileName);
-            }
-            else if (ans == "n" || ans == "N")
-            {
-                System.Console.WriteLine("Closing the app.");
-                Environment.Exit(1);
-            }
-            else
-            {
-                System.Console.WriteLine("Invalid input, please use y, Y, n or N. Please try again.");
-                System.Console.WriteLine();
-            }
-        }
-    }
-    //Executing the main menu options which are: Listing, Updating, Adding and removing.
-    while (true)
-    {
-        System.Console.WriteLine("\nPlease select one of below options:\n 1 - List all teachers\n 2 - Add a new teacher\n 3 - Remove a teacher\n 4 - Update an existing teacher" +
-            "\n 5 - Close application");
-        string ans = Console.ReadLine();
-        if (ans == null || !"12345".Contains(ans))
-        {
-            System.Console.WriteLine("Invalid option, please select one of the valid options below.");
-        }
-        else
-        {
-            switch (ans)
-            {
-                case "1":
-                    TeacherInterface.listTeachers(listTeachers);
-                    break;
-                case "2":
-
-                    break;
-                case "3":
-
-                    break;
-                case "4":
-
-                    break;
-                case "5":
-                    string[] textToFile = genListToSave(listTeachers);
-                    File.Delete(fileName);
-
-                    updateFile(fileName, textToFile);
-                    Environment.Exit(1);
-                    break;
-            }
-        }
-    }
-
-
-
-}
